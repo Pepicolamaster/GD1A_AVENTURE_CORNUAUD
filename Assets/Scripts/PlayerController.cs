3c1b7e29a44e83d863c98721b709a037d3dfb402
Mathys Cornuaud <mathys.cornuaud85@gmail.com>
2024-03-18 10:28:21 +0100

    Player Controls & Scene Loading

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    //movement directionnel
+    private float horizontalInput;
+    private float verticalInput;
+    public float speed;
+    public bool hasCoin = false;
+
+    //références
+
+    private void Update()
+    {
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+        transform.Translate(Vector2.right * horizontalInput * speed * Time.deltaTime);
+        verticalInput = Input.GetAxisRaw("Vertical");
+        transform.Translate(Vector2.up * verticalInput * speed * Time.deltaTime);
+        /**transform.Transform = bouger les coordonnées du joueur sans physique, donc sans RB2D.
+        Time.deltaTime permet de cnvertir la vitesse en temps réel*/
+    }
+
+    public void AddCoin()
+    {
+        hasCoin = true;
+    }
+}
